204bb0d453643e11abcf492f768d59c1543dcdfe
16110347 <afdhal.luthfi@students.amikom.ac.id>
2019-02-22 11:15:34 +0700

    logic calculator

---
+++ calculator.js
@@ -0,0 +1,40 @@
+var calculator = {
+
+    tambah: function tambah(x, y) {
+        var x = x;
+        var y = y;
+        var total = x + y;
+        return total;
+
+    },
+
+    kurang: function kurang(x, y) {
+        var x = x;
+        var y = y;
+        var total = x - y;
+        return total;
+
+    },
+
+    kali: function kali(x, y) {
+        var x = x;
+        var y = y;
+        var total = x * y;
+        return total;
+
+    },
+
+    bagi: function bagi(x, y) {
+        var x = x;
+        var y = y;
+        var total = x / y;
+        return total;
+
+    }
+
+
+
+
+
+
+}
